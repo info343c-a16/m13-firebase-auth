27fb5c96e96405789102fca2898c2514839b5a1f
mkfreeman <michael.k.freeman@gmail.com>
2016-10-24 14:40:59 -0700

    Removed answers from master branch

--- exercise-1/js/authenticate.js
+++ exercise-1/js/authenticate.js
@@ -2,92 +2,47 @@
 $(function() {
 
     // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyAviKZmMKsPQMwYlm-kPHOkv5Qs9OWEyQg",
-        authDomain: "m13-authentication.firebaseapp.com",
-        databaseURL: "https://m13-authentication.firebaseio.com",
-        storageBucket: "m13-authentication.appspot.com",
-        messagingSenderId: "429520713968"
-    };
-    firebase.initializeApp(config);
 
     // Sign Up: Function to create account on firebase, then redirect to index.html
     var signUp = function() {
         // Get email, password, and display name
-        var email = $('#email').val();
-        var password = $("#password").val();
-        var displayName = $('#display-name').val();
+
 
         // Create user, then set the user's display name
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(function(user) {
+
                 // Set display name
-                user.updateProfile({
-                    displayName: displayName
-                }).then(function() {
-                    window.location = '/';
-                });
-            }).catch(function(error) {
-                alert(error.message);
-            });
+
     };
 
     // SignIn: Function to authenticate on Firebase, then redirect to index.html
     var signIn = function() {
         // Get email and password
-        var email = $('#email').val();
-        var password = $("#password").val();
+
 
         // Authenticate using email and password, then redirect
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(function() {
-                window.location = '/';
-            })
-            .catch(function(error) {
-                alert(error);
-            });
+
     };
 
     // Sign out: Function to log a user out of firebase
     var signOut = function() {
         // Sign out, then redirect
-        firebase.auth().signOut().then(function() {
-            window.location = 'sign-up.html';
-        });
+
+
+
     };
 
     // Assign event lister to form submission
-    $('form').on('submit', function(event) {
-        event.preventDefault();
-        var formId = $(this).attr('id');
-        if (formId == 'sign-up') {
-            signUp();
-        } else if (formId == 'sign-in') {
-            signIn();
-        }
-    });
+
+
 
     // Assign click event to logout button
-    $('#log-out').on('click', function() {
-        signOut();
-    });
+
+
 
     // Authentication Change: see if a user is already signed in, and redirect
-    var checked;
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (checked !== true) {
+
             // Rediriect to index.html if there is a user and the pathname isn't '/'
-            if (user && window.location.pathname != '/') {
-                window.location = '/';
-            }
 
             // Redirect to sign-in if there is NOT a user and the pathname IS '/'
-            if (!user && window.location.pathname == '/') {
-                window.location = 'sign-in.html';
-            }
-            checked = true;
-        }
-    });
-
 
 });
--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -2,42 +2,22 @@
 
 $(function() {
     // Chats reference
-    var chats = firebase.database().ref('chats');
+
 
     // Listen for children being added:fired once on load, then each time a child is added
-    chats.on('child_added', function(snapshot) {
-        var data = snapshot.val();
-        // Pass key, data to snapshot function
-        renderChat(snapshot.key, data);
-    });
+
+
 
     // Function to render each chat
     var renderChat = function(id, data) {
-        // Store each chat in a div
-        var newChat = $('<div>');
-
-        // Add a span of the username with class "user"
-        var name = $('<span>').addClass('user').text(data.name + ': ');
-
-        // Add a span that has the text of the chat
-        var message = $("<span>").text(data.chat);
-
-        // Append element to the page
-        newChat.append(name);
-        newChat.append(message);
-        $('#chat-box').append(newChat);
+        // Build appropriate DOM elements and add them to the DOM
+        
     };
 
 
     // Assign event listener to the chat button
-    $('#chat-button').on('click', function() {
 
         // Push in a new element to the chats reference
-        chats.push({
-            name: firebase.auth().currentUser.displayName,
-            chat: $("#message").val()
-        });
-        $('#message').val('');
-    });
+
 
 });
